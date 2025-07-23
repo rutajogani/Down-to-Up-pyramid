8400651633bcc1c1be955132bf12004636f774b7
Ruta Jogani <ruta007jogani@gmail.com>
2025-07-23 18:16:45 +0530

    add: more lines

---
+++ pyramid.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+
+    for (int i = 0; i < number; i++)
+    {
+        for (int j = number; j >= i; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}
